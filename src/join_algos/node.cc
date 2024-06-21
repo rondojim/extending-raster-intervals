e700c3808a8a44f81a5dd92c87fd7d7a068ba099
rondojim <dimronto@gmail.com>
2024-06-21 13:02:02 +0300

    Remove everything for fresh start

--- src/join_algos/node.cc
+++
@@ -1,9 +0,0 @@
-#include "../../include/join_algos/node.h"
-#include <cassert>
-
-NodeY::NodeY()
-    : active_polygons(false), whole(false), left(nullptr), right(nullptr) {}
-
-NodeX::NodeX() : left(nullptr), right(nullptr), rootY_whole(nullptr) {
-  rootY = new NodeY();
-}
