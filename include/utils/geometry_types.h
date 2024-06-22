3089fda8a7d6cd99c47172ac7d88c5af5c3bdf67
rondojim <dimronto@gmail.com>
2024-06-22 09:42:23 +0300

    Fix mistake in check intersection

--- include/utils/geometry_types.h
+++ include/utils/geometry_types.h
@@ -33,11 +33,11 @@ struct Polygon {
   std::vector<const Point *> vertices;
   int polygon_id;
 
-  Point minCorner; // for mbr 
+  Point minCorner; // for mbr
   Point maxCorner; // for mbr
 
-  //find the mbr of the vertices and set it to minCorner, maxCorner
-  void findMBR(); 
+  // find the mbr of the vertices and set it to minCorner, maxCorner
+  void findMBR();
 
   // save polygon vertices in output_file in given mode
   // x0 y0
@@ -48,52 +48,53 @@ struct Polygon {
   // prints the vertices of the polygon
   void print_();
 
-
   bool intersects(const Polygon &other) const;
   bool point_inside(const Point &p) const;
 
   // return string containing the WKT representation of the polygon.
-  std::string to_wkt() const ;
+  std::string to_wkt() const;
 };
 
 // returns polygon area using shoelace algo
 double polygon_area(const std::vector<const Point *> &vertices);
 
- /* The result of this cross product can be used to determine the relative 
-    orientation of the points:
- - If the result is positive, then p1p2 to p1p3 makes a counterclockwise turn.
- - If the result is negative, then p1p2 to p1p3 makes a clockwise turn.
- - If the result is zero, then the points are collinear.
+/* The result of this cross product can be used to determine the relative
+   orientation of the points:
+- If the result is positive, then p1p2 to p1p3 makes a counterclockwise turn.
+- If the result is negative, then p1p2 to p1p3 makes a clockwise turn.
+- If the result is zero, then the points are collinear.
 return The cross product of vectors p1p2 and p1p3. */
 double cross_product(const Point &p1, const Point &p2, const Point &p3);
 
 // returns true of p1, p2, p3 are collinear, else false
 bool are_collinear(const Point &p1, const Point &p2, const Point &p3);
 
-// calculates the x-coordinate where two lines, defined by 
-// the points (p1, p2) and (p3, p4), intersect. If the lines are parallel 
+// calculates the x-coordinate where two lines, defined by
+// the points (p1, p2) and (p3, p4), intersect. If the lines are parallel
 // or collinear, the function returns `NaN` to indicate no valid intersection.
 double x_intersect_(const Point &p1, const Point &p2, const Point &p3,
                     const Point &p4);
 
-// calculates the y-coordinate where two lines, defined by 
-// the points (p1, p2) and (p3, p4), intersect. If the lines are parallel 
+// calculates the y-coordinate where two lines, defined by
+// the points (p1, p2) and (p3, p4), intersect. If the lines are parallel
 // or collinear, the function returns `NaN` to indicate no valid intersection.
 double y_intersect_(const Point &p1, const Point &p2, const Point &p3,
                     const Point &p4);
 
-// calculates the point where two lines, defined by 
-// the points (p1, p2) and (p3, p4), intersect. If the lines are parallel 
+// calculates the point where two lines, defined by
+// the points (p1, p2) and (p3, p4), intersect. If the lines are parallel
 // or collinear, the function returns nullptr to indicate no valid intersection.
 Point *p_intersect(const Point &p1, const Point &p2, const Point &p3,
                    const Point &p4);
 
 // Function to save a list of polygons to a CSV file
 // in wkt
-void save_polygons_to_csv(const std::vector<Polygon>& polygons, const char* output_file);
+void save_polygons_to_csv(std::vector<Polygon> &polygons,
+                          const char *output_file);
 
 // get total area of polygons in vertices_vectors
-double get_polygons_area(std::vector<std::vector<const Point *>> &vertices_vectors);
+double
+get_polygons_area(std::vector<std::vector<const Point *>> &vertices_vectors);
 
 // print vertices of vec
 void print_vec(const std::vector<const Point *> &vec);
@@ -101,6 +102,6 @@ void print_vec(const std::vector<const Point *> &vec);
 // if p1, p2 ara parallel to x axis return 1
 // else if on y axis return 2
 // else 0
-unsigned int checkParallelism(const Point& p1, const Point& p2);
+unsigned int checkParallelism(const Point &p1, const Point &p2);
 
 #endif
